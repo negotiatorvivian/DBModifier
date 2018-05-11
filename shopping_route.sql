2d04c8b0229eebbf7cc3bfe910692872d6e814b7
zhangziwe <you@example.com>
2018-05-11 11:21:48 +0800

    add table negetive_samples

--- shopping_route.sql
+++ shopping_route.sql
@@ -41,3 +41,17 @@ create table categories (
 
 alter table categories add column clicks int default 0 comment '用户偏好的类别计数' after cat_name;
 
+
+create table nagetive_samples (
+    id int auto_increment primary key comment '自增主键',
+    age int default 0 comment '年龄',
+    gender int default 0 comment '0:男, 1:女',
+    occupation int default 1 comment '职业',
+    city varchar(10) default 'A' comment '城市类别',
+    years varchar(10) default '0' comment '在当前城市呆的时间',
+    marital_status int default 0 comment '婚姻状态,0:未婚, 1:已婚',
+    prefer_cats varchar(20) default '' comment '用户喜欢的类别',
+    create_time int default 0 comment '创建时间',
+    update_time int default 0 comment '更新时间',
+    status int default 1 comment '状态,1:合法, 0:非法'
+) engine = innodb default charset = utf8mb4 collate = utf8mb4_unicode_ci;
